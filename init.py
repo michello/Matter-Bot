a8d034a3a48cae680b894f0a9f56b483dd2ded62
Michelle <ml4963@nyu.edu>
2018-06-27 15:57:48 -0400

    adding slack stuff

--- init.py
+++ init.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from appdef import app
 ## each page will have its own python file/module, which will be imported from here:
-import main
+import main, slack
 
 if __name__ == "__main__":
     app.run('localhost', 5000, debug = True)
--- slack.py
+++ slack.py
@@ -1,15 +1,20 @@
-#from slackclient import SlackClient
+from slackclient import SlackClient
 
 slack_token = open("slack_creds.txt","r").readline().strip()
-print(slack_token)
 
-'''
-slack_token = os.environ[]
 sc = SlackClient(slack_token)
 
+'''
 sc.api_call(
   "chat.postMessage",
-  channel="C0XXXXXX",
+  channel="recommendations",
   text="Hello from Python! :tada:"
 )
 '''
+
+sc_history = sc.api_call(
+  "conversations.history",
+  channel="CBEDJTJUQ"
+)
+
+print(sc_history['messages'])
