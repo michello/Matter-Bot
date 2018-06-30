a0975e49ca6b0e49e7cee5fe4aaed08f8ffa8269
Dandan Lin <dandan.lin@blueapron.com>
2018-06-30 15:39:55 -0400

    Adding Reply

--- main.py
+++ main.py
@@ -6,54 +6,63 @@ from appdef import app
 import os
 from twilio.rest import Client
 from slackclient import SlackClient
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, session
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+SECRET_KEY = os.environ['SECRET_KEY']
+SLACK_TOKEN = os.environ['SLACK_TOKEN']
 
 TWILIO_SID = os.environ['TWILIO_SID']
 TWILIO_TOKEN = os.environ['TWILIO_TOKEN']
-SLACK_TOKEN = os.environ['SLACK_TOKEN']
- 
+
 # Create Twilio and Slack REST client object
 t_client = Client(TWILIO_SID, TWILIO_TOKEN)
 s_client = SlackClient(SLACK_TOKEN)
-  
+
+employee_name = ['DANDAN LIN', 'MICHELLE LAM', 'RICHARD WU', 'HUI WAH CHIANG']
+employee_id =['0702', '0305', '7853', '0127', '0629', '0521']
+job_title = ['RUNNER', 'ASSOCIATE', 'QA ASSOCIATE', 'KITTER', 'SPECIALIST', 'MACHINE OPERATOR', 'TECH', 'CUSTODIAN']
+
 @app.route("/", methods=['GET'])
 def main():
     return render_template("index.html")
 
 @app.route("/incoming_sms", methods=['GET', 'POST'])
 def incoming_sms():
-  # TODO: Make it two way 
-  if request.method == 'POST':
-    resp = MessagingResponse()
-    resp.message("""What is your First Name and Last Name?""")
-  
-    return str(resp)
-  # elif request.method == 'GET':?
-    
-  
-  # if request.method == 'GET':
-  #   # Retrieve the message list
-  #   message = t_client.messages.list()
+  resp = MessagingResponse()
 
-  #   for sms in message:
-  #     if (sms.to == "+12012989124"):
-  #       s_client.api_call(
-  #         "chat.postMessage",
-  #         channel="recommendations",
-  #         text=sms.body
-  #       )
-  #       return sms.body
-  # elif request.method == 'POST':
-  #   # Start our TwiML response
-  #   resp = MessagingResponse()
-  #   resp.message("Ahoy! Thanks so much for your message nnnsd. ")
-  #   return str(resp)
-  # else:
-  #   resp.message("Ahoy! Thanks so much for your message.")
-  #   return str(resp)
+  if request.method == 'POST':
+    counter = session.get('counter', 0)
+    counter += 1
+
+    # Save the new counter value in the session
+    session['counter'] = counter
+    message_from = request.values.get('From')
+    message_body = request.values.get('Body').upper()
+
+    message = ''
+
+    if message_body == 'HI':
+      message = 'Hi, what is your First Name and Last Name?'
+    elif message_body in employee_name :
+      message = "Thank you " + message_body +", please enter your Employee ID"
+    elif message_body in employee_id:
+      message = "What is your job title?"
+    elif message_body in job_title:
+      message = "Thank you. You now can share your idea with us \nSend \'Done\' when you finish"
+    elif message_body == 'DONE':
+      message = "Thank you for using Matter Bot. Have a great day"
+    # else:
+      # TODO: where to save the data?
+
+
+    resp.message(message)
+    return str(resp)
 
 if __name__ == "__main__":
+  app.secret_key = SECRET_KEY
   app.run(debug=True)
+  app.run()
