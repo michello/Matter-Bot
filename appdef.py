3eee4395a042ab7706e7680c1eb934db990e6211
Dandan Lin <dandan.lin@blueapron.com>
2018-06-30 17:35:43 -0400

    Teating

--- appdef.py
+++ appdef.py
@@ -1,21 +1,20 @@
-from flask import *
-# import sms
-#from db_connection import *
-
-# for database connection if needed
 import os
-#import SMSOutgoing
+import pymysql
+import pymysql.cursors
+
+from flask import *
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 app = Flask(__name__)
 # db = MyDB()
 
-conn = pymysql.connect(
-  host='localhost',
-  port=8080,
-  user='root',
-  password='',
-  db='wms_request_pipeline',
-  charset='utf8mb4',
-  cursorclass=pymysql.cursors.DictCursor)
+# conn = pymysql.connect(
+#   host='localhost',
+#   user='root',
+#   password='',
+#   db='wms_request_pipeline',
+#   charset='utf8mb4',
+#   cursorclass=pymysql.cursors.DictCursor)
